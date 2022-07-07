2ca0c369efdcdb33b56d4cc80e32b98e63b0ae69
Anthony Devine <antdevine@hotmail.co.uk>
2022-07-07 21:44:48 +0100

    banner component

---
+++ src/components/Banner.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h2 v-if="title" v-html="title" />
+        <slot></slot>
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Banner',
+    props: {
+        title: {
+            type: String,
+            required: false
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+    h2 {
+        color: yellowgreen;
+        background: blue;
+    }
+</style>
